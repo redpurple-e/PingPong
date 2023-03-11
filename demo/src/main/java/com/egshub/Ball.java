fe14c39f0c31a81762ca28f533f282a04efa7800
Ezequiel <egs.cttprofissional@gmail.com>
2023-03-11 19:47:35 -0300

    Collision update

--- demo/src/main/java/com/egshub/Ball.java
+++ demo/src/main/java/com/egshub/Ball.java
@@ -2,7 +2,9 @@ package com.egshub;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Ball {
     public double x, y;
@@ -10,20 +12,63 @@ public class Ball {
     public static int width = 4;
     public static int height = 4;
 
-    public double dx, dy, speed;
+    private int offset = 1;
+
+    public double dx, dy;
+    public double speed = 1;
 
     public Ball(double x, double y) {
-        this.x = x;
-        this.y = y;
+        this.x = x + width/2;
+        this.y = y + height/2;
 
         dx = new Random().nextGaussian();
         dy = new Random().nextGaussian();
-        speed = 0.5;
     }
     
     public void tick() {
+        
+        double mag = Math.sqrt(dx*dx + dy*dy);
+        dx /= mag;
+        dy /= mag;
+
+        if(dx * speed + x > Game.WIDTH - width - offset) {
+            dx *= -1;
+            Game.enemy.chance = new Random().nextInt(99);
+        }
+
+        if(dx * speed + x < offset) {
+            dx *= -1;
+            Game.enemy.chance = new Random().nextInt(99);
+        }
+
+        Rectangle bounds = new Rectangle((int) (dx * speed + x), (int) (dy * speed + y), width, height);
+
+        Rectangle boundsPlayer = new Rectangle(Game.player.x, Game.player.y, Game.player.width, Game.player.height);
+        Rectangle boundsEnemy = new Rectangle((int) (Game.enemy.x), (int) (Game.enemy.y), Game.enemy.width, Game.enemy.height);
+
+        if(bounds.intersects(boundsPlayer)) {
+            dy *= -1;
+            Game.enemy.chance = new Random().nextInt(99);
+        }
+        if(bounds.intersects(boundsEnemy)) {
+            dy *= -1;
+            Game.enemy.chance = new Random().nextInt(99);
+        }
+
         x += dx * speed;
         y += dy * speed;
+        
+        if(y > Game.HEIGHT - offset || y < offset) {
+            // ponto do inimigo
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            new Game();
+        }
+
     }
 
     public void render(Graphics g) {
--- demo/src/main/java/com/egshub/Enemy.java
+++ demo/src/main/java/com/egshub/Enemy.java
@@ -2,29 +2,32 @@ package com.egshub;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Enemy {
 
     public double x, y;
+    public double speed = 4;
 
-    public static int width = 25;
-    public static int height = 4;
+    public int width = 25;
+    public int height = 4;
 
     private int offset = 1;
-
+    public double chance = new Random().nextInt(9);
+    
     public Enemy(double x, double y) {
-        this.x = x;
-        this.y = y;
+        this.x = x - width/2;
+        this.y = y + offset;
     }
     
     public void tick() {
-        x += Game.ball.x - x;
-
         
+        x += (Game.ball.x - x) * (chance + 10) / 100;
+
         if(x > Game.WIDTH - width - offset) {
             x = Game.WIDTH - width - offset;
         }
-
+        
         if(x < offset) {
             x = offset;
         }
